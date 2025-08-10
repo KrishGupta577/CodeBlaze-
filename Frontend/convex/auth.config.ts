206770724319a9639efdfc66a58dced1779c62d8
krishgupta577 <krishgupta577@gamil.com>
2025-08-10 22:57:48 +0530

    chanes

--- Frontend/convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    providers: [
-        {
-            domain: "https://enough-thrush-28.clerk.accounts.dev",
-            applicationID: "convex",
-        },
-    ]
-};
--- Frontend/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function UserRootRedirect() {
-    redirect("/user");
-}
